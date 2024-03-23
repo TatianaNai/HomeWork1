6636fa07301c143b409a6d6ad3de6abc3f6f1a2d
Tatiana <134440396+TatianaNai@users.noreply.github.com>
2024-03-23 17:25:06 +0300

    Add files via upload

---
+++ HomeWork1/Calculator.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1
+{
+    internal class Calculator
+    {
+        public static bool IsOperator(char sybmol)
+        {
+            if (sybmol == '+' || sybmol == '-' || sybmol == '/' || sybmol == '*' || sybmol == '%')
+                return true;
+            else return false;
+        }
+
+        public static void GetResult(double number1, double number2, char mathOperator)
+        {
+            double result;
+            switch (mathOperator)
+            {
+                case '+':
+                    result = Math.Round(number1 + number2, 2);
+                    Console.WriteLine("Результат = " + result);
+                    break;
+                case '-':
+                    result = Math.Round(number1 - number2, 2);
+                    Console.WriteLine("Результат = " + result);
+                    break;
+                case '/':
+                    if (number2 == 0)
+                    {
+                        Console.WriteLine("На ноль делить нельзя!");
+                        break;
+                    }
+                    else
+                    {
+                        result = Math.Round(number1 / number2, 2);
+                        Console.WriteLine("Результат = " + result);
+                        break;
+                    }
+                case '*':
+                    result = Math.Round(number1 * number2, 2);
+                    Console.WriteLine("Результат = " + result);
+                    break;
+                case '%':
+                    if (number1 < 0 || number2 < 0)
+                    {
+                        Console.WriteLine("Введите положительное число");
+                        break;
+                    }
+                    else
+                    {
+                        result = Math.Round(number1 * number2 / 100, 2);
+                        Console.WriteLine("Результат = " + result);
+                        break;
+                    }
+                case 's':
+                    if (number1 < 0)
+                    {
+                        Console.WriteLine("Введите положительное число");
+                        break;
+                    }
+                    else
+                    {
+                        result = Math.Round(Math.Sqrt(number1), 2);
+                        Console.WriteLine("Результат = " + result);
+                        break;
+                    }
+            }
+        }
+    }
+}
---
+++ HomeWork1/Program.cs
@@ -0,0 +1,54 @@
+﻿
+namespace HomeWork1
+{
+    class Program
+    {
+        public const int sqrtLength = 4;
+
+        public static void Main(string[] args)
+        {
+            double number1 = 0;
+            double number2 = 0;
+            string stringNumber1;
+            string stringNumber2;
+
+            bool isNumber1 = false;
+            bool isNumber2 = false;
+            char mathOperator = default;
+
+            do
+            {
+                Console.WriteLine("Введите выражение");
+                string mathExpression = Console.ReadLine();
+
+                if (mathExpression.Contains("sqrt"))
+                {
+                    stringNumber1 = mathExpression.Substring(sqrtLength, mathExpression.Length - sqrtLength);
+                    isNumber1 = Double.TryParse(stringNumber1, out number1);
+                    isNumber2 = true;
+                    mathOperator = 's';
+                }
+                else
+                {
+                    for (int i = 1; i < mathExpression.Length - 1; i++)
+                    {
+                        if (Calculator.IsOperator(mathExpression[i]))
+                        {
+                            mathOperator = mathExpression[i];
+
+                            stringNumber1 = mathExpression.Substring(0, i);
+                            stringNumber2 = mathExpression.Substring(i + 1, mathExpression.Length - (i + 1));
+
+                            isNumber1 = Double.TryParse(stringNumber1, out number1);
+                            isNumber2 = Double.TryParse(stringNumber2, out number2);
+                            break;
+                        }
+                    }
+                }
+            }
+            while (!isNumber1 || !isNumber2);
+
+            Calculator.GetResult(number1, number2, mathOperator);
+        }
+    }
+}
